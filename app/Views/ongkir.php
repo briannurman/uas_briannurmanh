ee965892581924b9104114a8e5b31e1b05ffe500
briannurman <briannurman31@gmail.com>
2021-07-27 22:56:47 +0700

    Update ongkir.php

--- app/Views/ongkir.php
+++ app/Views/ongkir.php
@@ -138,13 +138,13 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Dialog Hasil</h5>
+                        <h5 class="modal-title text-success" id="exampleModalLabel">Dialog Hasil</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        <p class="text-center text-success">Biaya yang diperlukan adalah</p>
+                        <p class="text-success text-center ">Biaya yang diperlukan adalah</p>
                         <h2 name="ongkir" id="ongkir" class="text-success text-center"></h2>
                     </div>
                     <div class="modal-footer">
